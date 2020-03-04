92d5c25f9b3df962054710dbb3cd2404f0daa293
Dmitriy Pechenkin <dmitry.pechenkin@firstlinesoftware.com>
2020-03-04 03:26:20 +0300

    fix

--- src/main/java/com/dmitriy/shortener/service/impl/UrlShortenerServiceImpl.java
+++ src/main/java/com/dmitriy/shortener/service/impl/UrlShortenerServiceImpl.java
@@ -3,15 +3,11 @@ package com.dmitriy.shortener.service.impl;
 import com.dmitriy.shortener.model.UrlStorageEntity;
 import com.dmitriy.shortener.repository.JpaUrlStorageRepository;
 import com.dmitriy.shortener.service.UrlShortenerService;
-import com.google.common.hash.Hashing;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.nio.charset.StandardCharsets;
 
 @Service
 @Transactional
@@ -21,14 +17,7 @@ public class UrlShortenerServiceImpl implements UrlShortenerService {
     private final JpaUrlStorageRepository jpaUrlStorageRepository;
 
     @Override
-    @Caching(
-            cacheable = {
-                    @Cacheable(value = "originalUrl", key = "#entity.originalUrl")
-            },
-            evict = {
-                    @CacheEvict(value = "shortUrl", key = "#entity.shortUrl")
-            }
-    )
+    @CachePut(value = "shortUrl", key = "#entity.shortUrl")
     public String createShortUrl(UrlStorageEntity entity) {
 
         UrlStorageEntity result = jpaUrlStorageRepository.findByOriginalUrl(entity.getOriginalUrl());
